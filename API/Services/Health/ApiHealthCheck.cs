266b9d90d67059c1716f427249702225d76259cd
khattab88 <m_khatab_88@hotmail.com>
2023-11-24 22:46:57 +0200

    implement health checks

---
+++ API/Services/Health/ApiHealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using RestSharp;
+
+namespace API.Services.Health
+{
+    public class ApiHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var url = "https://localhost:5002/WeatherForecast";
+
+            var client = new RestClient();
+
+            var request = new RestRequest(url, Method.Get);
+            request.AddHeader("Accept", "application/json");
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
+            {
+                return HealthCheckResult.Healthy();
+            }
+            else
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Implementing health checks in .NET 7 Web API.
 
+## Steps:
+1. Navigate to {API_URL}/health (Ex: https://localhost:5001/health)
+
 
 #Reference: https://www.youtube.com/watch?v=MMk3V58pNIw&ab_channel=MohamadLawand
